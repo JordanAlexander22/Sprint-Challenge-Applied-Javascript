f160796216a1f3cde7d52c454e0aeb37c987685f
jordan <jordan@jordans-Air.lan>
2019-11-07 12:35:29 +0700

    added header

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,29 +11,33 @@
 // const headerContainer= document.querySelector('.header-container');
 
 // const headerElements= Header() 
-
 // headerContainer.appendChild(headerElements)
 
-// function Header() {
-//     const header = document.createElement('div')
-//     const date = document.createElement('span')
-//     const title = document.createElement('h1')
-//     const temp = document.createElement('span')
 
-//     date.textContent = 'March 28, 2019'
-//     title.textContent = 'Lambda Times'
-//     temp.textContent = '98°'
+function HeadTitle(){ //made a function called HeadTtile
+    const top = document.createElement('div');
+    const date = document.createElement('span');
+    const title = document.createElement('h1');
+    const temp = document.createElement('span');  //grabbed all elements from given index and CreatedElement tag
 
-//     header.classList.add('header')
-//     date.classList.add('date')
-//     temp.classList.add('temp')
-    
-//     header.appendChild(date)
-//     header.appendChild(title)
-//     header.appendChild(temp)
+    date.textContent = 'MARCH, 28 2019'; 
+    title.textContent =  'Lambda Times'
+    temp.textContent = '98°'
 
-//     console.log(header)
 
-//     return header
-// }
+    top.classList.add('header');
+    date.classList.add('date');  //added using classList header, temp and date
+    temp.classList.add('temp');
 
+    top.appendChild(date);
+    top.appendChild(title);  //appeneded child. 
+    top.appendChild(temp);
+
+    console.log(HeadTitle); //consoled log the HeadTitle from the function 
+
+    return top
+}
+
+const containHead = document.querySelector('.header-container'); //did a query selector to grab all what was in the .header-container
+const elementHead = HeadTitle()
+containHead.append(elementHead)
